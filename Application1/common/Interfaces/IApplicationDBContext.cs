3f4e9f17e633fb4f1da70570305705979afb0de1
Loc <Loc@DESKTOP-TUECCOQ>
2023-03-23 10:53:20 +0700

    Add project files.

---
+++ Application1/common/Interfaces/IApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.common.Interfaces
+{
+    public interface IApplicationDBContext
+    {
+        DbSet<AppSetting> AppSettings { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
